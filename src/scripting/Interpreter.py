89c2aaf95ed5f1194a4299821590aa49268cc44f
Franco030 <edwinfranco3104@hotmail.com>
2025-12-12 16:43:35 -0600

    added new programming language, but I still don't know how I can use it

--- src/scripting/Interpreter.py
+++ src/scripting/Interpreter.py
@@ -1,4 +1,4 @@
-from src.scripting.Lexer import Lexer, TokenType
+from src.scripting.Lexer import TokenType
 from src.scripting.AST import *
 from src.utils.Game_Enums import Actions
 from src.core.GameState import game_state
@@ -18,11 +18,9 @@ class Environment:
     def get(self, name):
         if name in self.values:
             return self.values[name]
-        
         if self.parent:
             return self.parent.get(name)
-        
-        raise Exception(f"Variable '{name}' not defined")
+        raise Exception(f"Variable '{name}' no definida.")
 
 class Interpreter:
     def __init__(self, action_manager, player, scene):
@@ -30,7 +28,6 @@ class Interpreter:
         self.player = player
         self.scene = scene
         self.functions = {}
-
         self.globals = Environment()
         self.environment = self.globals
 
@@ -39,7 +36,8 @@ class Interpreter:
             "show_dialogue": (Actions.SHOW_DIALOGUE, ["text"]),
             "show_note":     (Actions.SHOW_NOTE, ["id"]),
             "wait":          (Actions.WAIT, ["time"]),
-            "teleport":      (Actions.TELEPORT, ["zone", "x", "y"])
+            "teleport":      (Actions.TELEPORT, ["zone", "x", "y"]),
+            # Add other functions here
         }
 
     def load(self, program_node):
@@ -47,29 +45,23 @@ class Interpreter:
             if isinstance(decl, FunctionDecl):
                 self.functions[decl.name] = decl
 
-    def run_function(self, name):
+    def run_function(self, name, args=[]):
         func_node = self.functions.get(name)
-        if not func_node:
-            print(f"[Interpreter] Function '{name}' was not found")
-            return
+        if not func_node: 
+            print(f"[Interpreter] Warning: Function '{name}' was not found")
+            return None
         
-        try:
-            return self.visit(func_node.body)
-        except ReturnException as e:
-            return e.value
-        
+        return self._execute_user_function(func_node, args)
+
     def _execute_user_function(self, func_node, arguments):
         previous_env = self.environment
         local_env = Environment(self.globals)
-
+        
         for i, param_name in enumerate(func_node.params):
-            if i < len(arguments):
-                local_env.define(param_name, arguments[i])
-            else:
-                local_env.define(param_name, None)
+            val = arguments[i] if i < len(arguments) else None
+            local_env.define(param_name, val)
 
         self.environment = local_env
-
         ret_val = None
         try:
             ret_val = self.visit(func_node.body)
@@ -77,34 +69,75 @@ class Interpreter:
             ret_val = e.value
         finally:
             self.environment = previous_env
-        
         return ret_val
-        
+
+    # --- VISIT ---
     def visit(self, node):
+        if node is None:
+            print("[Interpreter DEBUG] Attempt to visit a 'None' node. This indicates an error in the Parser or in the AST")
+            return None
+
         method_name = f'visit_{type(node).__name__}'
         visitor = getattr(self, method_name, self.generic_visit)
         return visitor(node)
 
     def generic_visit(self, node):
-        raise Exception(f"[Interpreter] there's no method visit_{type(node).__name__}")
-    
+        raise Exception(f"No existe método visit_{type(node).__name__}")
+
+    # --- NODES ---
+
     def visit_Block(self, node):
         last_result = None
-
         for stmt in node.statements:
-            res = self.visit(stmt)
-
-            if res is not None:
-                last_result = res
-
+            if stmt is None: continue
+            last_result = self.visit(stmt)
         return last_result
 
+    def visit_FunctionCall(self, node):
+        args = []
+        for i, arg in enumerate(node.arguments):
+            if arg is None:
+                print(f"[Interpreter ERROR] The argument #{i+1} of '{node.name}' is None.")
+                args.append(None)
+            else:
+                args.append(self.visit(arg))
+
+        if node.name == "get_flag":
+            return game_state.get_flag(args[0])
+        
+        if node.name in self.native_map:
+            return self._call_native(node.name, args)
+
+        if node.name in self.functions:
+            return self._execute_user_function(self.functions[node.name], args)
+
+        print(f"[Interpreter] Error: Function '{node.name}' unknown")
+        return None
+
+    def _call_native(self, name, args):
+        action_type, param_names = self.native_map[name]
+        param_string = ""
+        for i, val in enumerate(args):
+            if i < len(param_names):
+                param_string += f"{param_names[i]}={val};"
+        
+        print(f"[Interpreter] Native Call: {name}({param_string})")
+        return self.action_manager.execute(action_type, param_string, self.player, self.scene)
+
+    def visit_Literal(self, node):
+        if isinstance(node.value, str) and node.value not in self.native_map:
+            try:
+                return self.environment.get(node.value)
+            except:
+                return node.value
+        return node.value
+
     def visit_IfStatement(self, node):
-        condition = self.visit(node.condition)
-        if condition:
-            self.visit(node.then_branch)
+        if self.visit(node.condition):
+            return self.visit(node.then_branch)
         elif node.else_branch:
-            self.visit(node.else_branch)
+            return self.visit(node.else_branch)
+        return None
 
     def visit_ReturnStatement(self, node):
         value = None
@@ -112,60 +145,18 @@ class Interpreter:
             value = self.visit(node.value)
         raise ReturnException(value)
 
-    def visit_FunctionCall(self, node):
-        args = [self.visit(arg) for arg in node.arguments]
-        if node.name == "get_flag":
-            return game_state.get_flag(args[0])
-        
-        if node.name in self.native_map:
-            return self._call_native(node.name, args)
-        
-        if node.name in self.functions:
-            return self._execute_user_function(self.functions[node.name], args)
-        
-        print(f"[Interpreter] Error: Unknown function '{node.name}'")
-
-    def _call_native(self, name, args_nodes):
-        action_type, param_names = self.native_map[name]
-        param_string = ""
-
-        for i, arg_node in enumerate(args_nodes):
-            if i >= len(param_names): break
-
-            val = self.visit(arg_node)
-            key = param_names[i]
-            param_string += f"{key}={val};"
-
-        print(f"[Interpreter] Executing native: {action_type} with params [{param_string}]")
-        return self.action_manager.execute(action_type, param_string, self.player, self.scene)
-    
-    def visit_Literal(self, node):
-        if isinstance(node.value, str) and node.value not in self.native_map:
-            try:
-                return self.environment.get(node.value)
-            except:
-                return node.value
-            
-        return node.value
-    
     def visit_BinaryOp(self, node):
         left = self.visit(node.left)
         right = self.visit(node.right)
         op = node.operator
 
         if op == TokenType.PLUS:
-            if isinstance(left, str) or isinstance(right, str):
-                return str(left) + str(right)
+            if isinstance(left, str) or isinstance(right, str): return str(left) + str(right)
             return left + right
-            
         if op == TokenType.MINUS: return left - right
         if op == TokenType.MUL:   return left * right
-        if op == TokenType.DIV:   
-            if right == 0:
-                print("[Interpreter Runtime Error] Division by zero")
-                return 0
-            return left / right
-
+        if op == TokenType.DIV:   return 0 if right == 0 else left / right
+        
         if op == TokenType.GT: return left > right
         if op == TokenType.LT: return left < right
         if op == TokenType.EQUALS: return left == right
